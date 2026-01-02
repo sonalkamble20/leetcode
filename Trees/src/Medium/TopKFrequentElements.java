4015ff1e397f4ebb652e11cd4ec8a6e94eb21262
Sonal Kamble <sonalkamble@Sonals-MacBook-Pro-4.local>
2026-01-02 19:09:14 +0530

    top k frequent

--- Trees/src/Medium/TopKFrequentElements.java
+++ Trees/src/Medium/TopKFrequentElements.java
@@ -8,31 +8,29 @@ public class TopKFrequentElements {
         System.out.println(Arrays.toString(topKFrequent(new int[]{1, 2, 1, 2, 1, 2, 3, 1, 3, 2}, 2)));
     }
 
-    public static int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> frequency = new HashMap<>();
+    public static int[] topKFrequent(int[] nums, int k)
+        {
+            Map<Integer, Integer> freq = new HashMap<>();
 
-        for (int each : nums)
-            frequency.put(each, frequency.getOrDefault(each, 0) + 1);
+            for (int each : nums)
+                freq.put(each, freq.getOrDefault(each, 0) + 1);
 
-        List<Integer>[] list = new List[nums.length + 1];
+            List<Integer>[] list = new List[nums.length + 1];
 
-        for (int i = 0; i < list.length; i++)
-            list[i] = new ArrayList<>();
-
-        for (Map.Entry<Integer, Integer> entry : frequency.entrySet())
-            list[entry.getValue()].add(entry.getKey());
-
-        int[] res = new int[k];
-        int index = 0;
-        for (int i = list.length - 1; i > 0 && index < k; i--) {
-            for (int n : list[i]) {
-                res[index++] = n;
-                if (index == k) {
-                    return res;
-                }
+            PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> freq.get(b) - freq.get(a));
+            for (int n : freq.keySet())
+            {
+                pq.add(n);
             }
+
+
+            int[] result = new int[k];
+
+            for (int i = 0; i < k; i++) {
+                result[i] = pq.poll();
+            }
+            return result;
         }
-        return res;
     }
-}
+
 
