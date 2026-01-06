8bf379c8235f4853d6ec2f113899b9c80e158303
Sonal Kamble <sonalkamble209@gmail.com>
2026-01-06 18:32:21 +0530

    BST/ADT tree

---
+++ untitled/src/Trees/BinaryNode.java
@@ -0,0 +1,14 @@
+package Trees;
+
+public class BinaryNode
+{
+    int data;
+    BinaryNode left;
+    BinaryNode right;
+
+    public BinaryNode(int data, BinaryNode left, BinaryNode right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
