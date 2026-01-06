b609b531893ca22417c4078ffd56ca19ec866bd6
Sonal Kamble <sonalkamble209@gmail.com>
2026-01-06 18:35:16 +0530

    Valid Parenthesis

---
+++ NeetCode/src/Stack/ValidParenthesis.java
@@ -0,0 +1,42 @@
+//Valid Parentheses
+//You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+//The input string s is valid if and only if:
+//    Every open bracket is closed by the same type of close bracket.
+//    Open brackets are closed in the correct order.
+//    Every close bracket has a corresponding open bracket of the same type.
+//Return true if s is a valid string, and false otherwise.package Stack;
+package Stack;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static void main(String[] args)
+    {
+        System.out.println(isValid("[(])"));
+    }
+
+    public static boolean isValid(String s)
+    {
+        Stack<Character> valid = new Stack<>();
+
+        char[] arr = s.toCharArray();
+
+        for (char c : arr)
+        {
+            switch(c)
+            {
+                case '(':
+                case '{':
+                case '[': valid.push(c);
+                break;
+                case ')':
+                case '}':
+                case ']': valid.pop();
+                break;
+            }
+        }
+
+        return valid.isEmpty();
+    }
+}
